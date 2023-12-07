c37cd8169d4b82f91497c9f2f2a797b7f81b0511
beck <34204218+beck-8@users.noreply.github.com>
2023-12-07 22:03:17 +0800

    Create yixuebao.js

---
+++ scripts/yixuebao.js
@@ -0,0 +1,22 @@
+/*
+亿学宝云 提交满分答案
+
+***************************
+QuantumultX:
+
+[rewrite_local]
+^https:\/\/ccsfdx\.yxbyun\.com\/student\/studentPaper\/updateStudentPaperExamSubmit url script-request-body https://raw.githubusercontent.com/beck-8/QuantumultX/main/scripts/yixuebao.js
+
+[mitm]
+hostname = ccsfdx.yxbyun.com
+
+**************************/
+
+var obj = JSON.parse($response.body);
+obj.studentExamPaper.bigContent.forEach(bigContent => {
+    bigContent.smallContent.forEach(smallContent =>{
+      smallContent.question.studentAnswer = smallContent.question.questionAnswer
+    })
+});
+
+$done({body: JSON.stringify(obj)});
